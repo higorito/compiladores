e35d30c99ef26cc8b3f7045694fb68015f88288a
Yanky Jhonatha <88850852+Jhonatha1@users.noreply.github.com>
2023-11-05 23:57:57 -0300

    Update coisa de merda
    Outro pull request atualizado 23:57

--- python/lexico2.py
+++ python/lexico2.py
@@ -1,38 +1,33 @@
 class AnalisadorLexico:
-    def __init__(self, path:str):
-        self.__cab_leitura = 0
-        self.__linha = 1 
-        self.__percorrer = []
-        self.__lexema = ''
+    def __init__(self, path: str):
         self.__tabela_simbolos = []
+        self.__cab_leitura = 0
+        self.__linha = 1
+        self.__lexema = ''
         self.__arq_font = path
         self.__estado = 0
-        
 
-        self.__especiais = ['<', '>', ';', '[', ']']
-        self.__op_logicos = ['&&', '||', '!']
-        self.__op_relacionais = ['==', '!=', '>=', '<=', '>>', '<<']
-        self.__op_aritmeticos = ['+', '-', '*', '/', '//', '**']
+        self.__tokens_aritmeticos = ['+', '-', '*', '/', '//', '**']
+        self.__tokens_relacionais = ['==', '!=', '>=', '<=', '>>', '<<']
+        self.__tokens_logicos = ['&&', '||', '!']
+        self.__caracteres_especiais = ['<', '>', ';', '[', ']']
 
-        self.__palavras_reservadas = ['main','num_int', 'num_flu', 'text', 'case', 'ordo', 'to', 'when', 'tetin', 'texout', 'puts', 'take', 'fn', 'vacuum', 'bool']
+        self.__palavras_reservadas = ['main', 'num_int', 'num_flu', 'text', 'case', 'to', 'when', 'textin', 'textout', 'puts', 'take', 'fn', 'vacuum', 'bool']
 
-        self.__isComentario = ['--']
-        self.__isAtribuicao = ['->']
-        self.__isString = ['"'] 
+        self.__isComentario = '--'
+        self.__isAtribuicao = '->'
+        self.__isString = '"'
         self.__fim_linha = '\n'
         self.__fim_arquivo = '\0'
 
         arquivo = open(path, 'r')
         self._conteudo = arquivo.read()
-        
         arquivo.close()
-    
-    def __adicionar_token(self, tipo, lexema):
-        
-        token = {'tipo': tipo, 'lexema': lexema, 'linha': self.__linha}
-        self.__tabela_simbolos.append(token)
 
-    def get_tab_tokens(self):
+
+    
+    def get_tabela_simbolos(self):
+        tabela_simbolos = []
         while self.__cab_leitura < len(self._conteudo):
             char = self._conteudo[self.__cab_leitura]
 
@@ -46,7 +41,7 @@ class AnalisadorLexico:
                     self.__linha += 1
             elif self.__isAtribuicao and self._conteudo[self.__cab_leitura:self.__cab_leitura + 2] == '->':
                 # É um token de atribuição
-                self.__adicionar_token('Atribuição', '->')
+                self.adicionar_token('Atribuição', '->')
                 self.__cab_leitura += 2
             elif self.__isString and char == '"':
                 # É uma string
@@ -58,7 +53,7 @@ class AnalisadorLexico:
                     self.__cab_leitura += 1
                 self.__lexema += '"'
                 self.__cab_leitura += 1
-                self.__adicionar_token('String', self.__lexema)
+                self.adicionar_token('String', self.__lexema)
             elif char == self.__fim_linha:
                 self.__linha += 1
                 self.__cab_leitura += 1
@@ -72,9 +67,9 @@ class AnalisadorLexico:
                     self.__lexema += self._conteudo[self.__cab_leitura]
                     self.__cab_leitura += 1
                 if self.__lexema in self.__palavras_reservadas:
-                    self.__adicionar_token('Palavra Reservada', self.__lexema)
+                    self.adicionar_token('Palavra Reservada', self.__lexema)
                 else:
-                    self.__adicionar_token('Identificador', self.__lexema)
+                    self.adicionar_token('Identificador', self.__lexema)
             elif char.isdigit():
                 self.__estado = 2
                 self.__lexema = char
@@ -83,38 +78,47 @@ class AnalisadorLexico:
                     self.__lexema += self._conteudo[self.__cab_leitura]
                     self.__cab_leitura += 1
                 if '.' in self.__lexema:
-                    self.__adicionar_token('Número Flutuante', self.__lexema)
+                    self.adicionar_token('Número Flutuante', self.__lexema)
                 else:
-                    self.__adicionar_token('Número Inteiro', self.__lexema)
+                    self.adicionar_token('Número Inteiro', self.__lexema)
             else:
                 self.__estado = 3
                 self.__lexema = char
                 self.__cab_leitura += 1
 
-                if self.__lexema in self.__op_aritmeticos:
-                    self.__adicionar_token('Operador Aritmético', self.__lexema)
-                elif self.__lexema in self.__op_relacionais:
-                    self.__adicionar_token('Operador Relacional', self.__lexema)
-                elif self.__lexema in self.__op_logicos:
-                    self.__adicionar_token('Operador Lógico', self.__lexema)
-                elif self.__lexema in self.__especiais:
-                    self.__adicionar_token('Caractere Especial', self.__lexema)
-            self.__estado = 0
+                if self.__lexema in self.__tokens_aritmeticos:
 
-    
+                    self.adicionar_token('Operador Aritmético', self.__lexema)
+                elif self.__lexema in self.__tokens_aritmeticos:
+
+                    self.adicionar_token('Operador Relacional', self.__lexema)
+                elif self.__lexema in self.__tokens_logicos:
+                    self.adicionar_token('Operador Lógico', self.__lexema)
+                elif self.__lexema in self.__caracteres_especiais:
+                    self.adicionar_token('Caractere Especial', self.__lexema)
+                self.__estado = 0
+        return tabela_simbolos
+
+    def adicionar_token(self, tipo, lexema):
+        token = {'tipo': tipo, 'lexema': lexema, 'linha': self.__linha}
+        self.__tabela_simbolos.append(token)
+
+
     def main(self):
-
-        self.get_tab_tokens()
+        tabela_simbolos = self.get_tabela_simbolos()
         self.imprimir_tokens()
+
+
+
 
     def imprimir_tokens(self):
         for token in self.__tabela_simbolos:
             print(f'Tipo: {token["tipo"]}, Lexema: {token["lexema"]}, Linha: {token["linha"]}')
 
 
-if __name__ == "__main__":
-    
-    analisador = AnalisadorLexico("teste.txt")
-    analisador.main()
 
+
+if __name__ == "__main__":
+    analisador = AnalisadorLexico("teste.txt")
+    tabela_simbolos = analisador.get_tabela_simbolos()
     analisador.imprimir_tokens()
