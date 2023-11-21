8f060bc4629f80a0f4732a9e816eb84c5be2d7e4
Yanky Jhonatha <88850852+Jhonatha1@users.noreply.github.com>
2023-11-21 18:35:36 -0300

    Adicionando arquivos.

--- python/sintatico.py
+++ python/sintatico.py
@@ -1,3 +1,109 @@
+#### GRAMÁTICA ###
+
+# Início
+# main  ListaDeDeclaração  < escopo >
+
+# Lista De Declaração
+# ListaDeDeclaração | declaração | vacuum
+
+# Declaração
+# tipoVar variavel ;
+
+# TipoVar
+# text | numero 
+
+# Número
+# num_int | num_flu
+
+# Variável 
+# ID
+
+# Escopo 
+# escopo | comando | ε
+
+# Comando 
+# entrada | saida | desvio | atribuicao | laco
+
+# Entrada
+# textin [ variavel ]
+
+# Saída
+# textout [ texto | expAritmetica ]
+
+# Exp Aritmética 
+# exp | termo
+
+# Desvio 
+# case( exp ) < escopo > desvio2
+
+# Atribuição
+# variavel conteudo ;
+
+# Conteúdo
+# texto | exp
+
+# Laço
+# to( atribuicao conteudo simbolo_relacional conteudo ; variavel << expAritmetica ) < escopo > | when( exp ) < escopo > | take;
+
+# Exp
+# logico | ok | notok
+
+# Lógico
+# expressãoLogica termo logico
+
+# Termo
+# fator termo2
+
+# Termo2
+# *fator termo2 | / fator termo2 | // fator termo2 | ε
+
+# Fator
+# ( expAritmetica ) | variavel | numero | função
+
+# Função
+# ID ( argumento )
+
+# Argumento
+# expAritmetica argumento2 | ε
+
+# Argumento2
+# expAritmetica argumento2 | ε
+
+# Texto
+# text
+
+# Termo lógico
+# || expressaoLogica termoLogico | ε
+
+# Expressão Lógica
+# expressaoLogica3 expressaoLogica2
+
+# Expressão Lógica2
+# && expressaoLogica3 expressaoLogica2 | ε
+
+# Expressão Lógica3
+# ! relacional | relacional
+
+# Simbolo_relacional
+# << | >> | <<< | >>> | == | != | ok | notok
+
+# Desvio2
+# caseNot < escopo > | caseNot desvio3
+
+# Desvio3
+# case ( exp ) < escopo > desvio2 | ε
+
+# Relacional
+# logico | termoRelacional
+
+# TermoRelacional
+# conteudo termoRelacional2
+
+# TermoRelacional2
+# simbolo_relacional conteudo | ε
+
+
+
 from lexico2 import AnalisadorLexico
 
 class No:
@@ -34,40 +140,47 @@ class AnalisadorSintatico:
 
     def analisar_programa(self):
         try:
-            while self.indice_token_atual < len(self.tokens):
-                token_atual = self.tokens[self.indice_token_atual]
-                tipo_token_atual = token_atual['tipo']
-                lexema_token_atual = token_atual['lexema']
-
-                print(f"   Token encontrado - Tipo: {tipo_token_atual}, Lexema: {lexema_token_atual}, Linha: {token_atual['linha']}")
-                print("|________________________________________________________________________________|")
-                self.indice_token_atual += 1
-
+            self.analisar_escopo(self.raiz)
             print("\nAnálise dos tokens concluída com sucesso.")
+        except SyntaxError as e:
+            print(e)
 
         except SyntaxError as e:
             print(e)
 
+    def analisar_programa(self):
+        try:
+            self.analisar_escopo(self.raiz)
+            print("\nAnálise dos tokens concluída com sucesso.")
+        except SyntaxError as e:
+            print(e)
+
+
+
     def analisar_escopo(self, no_pai):
         no_escopo = No('Escopo')
-        while self.indice_token_atual < len(self.tokens):
-            token = self.tokens[self.indice_token_atual]['lexema']  # Correção aqui
-            if token in ('num_int', 'num_flu', 'text', 'bool'):
-                no_escopo.adicionar_filho(self.analisar_declaracao())
-            elif token == 'vacuum':
-                no_escopo.adicionar_filho(self.match('Palavra Reservada'))  # 'vacuum'
-            elif token == 'case':
-                no_escopo.adicionar_filho(self.analisar_desvio())
-            elif token == 'ordo':
-                no_escopo.adicionar_filho(self.analisar_ordo())
-            elif token == 'take':
-                no_escopo.adicionar_filho(self.match('Palavra Reservada'))  # 'take'
-                no_escopo.adicionar_filho(self.match('Num Inteiro'))  # '0'
-                self.match('Caractere Especial')  # ';'
-            else:
-                raise SyntaxError(f"Token inesperado {token} na linha {self.tokens[self.indice_token_atual]['linha']}")
+        try:
+            while self.indice_token_atual < len(self.tokens):
+                token = self.tokens[self.indice_token_atual]['tipo']
+                if token in ('num_int', 'num_flu', 'text', 'bool'):
+                    no_escopo.adicionar_filho(self.analisar_declaracao())
+                elif token == 'vacuum':
+                    no_escopo.adicionar_filho(self.match('Palavra Reservada'))  # 'vacuum'
+                elif token == 'case':
+                    no_escopo.adicionar_filho(self.analisar_desvio())
+                elif token == 'ordo':
+                    no_escopo.adicionar_filho(self.analisar_ordo())
+                elif token == 'take':
+                    no_escopo.adicionar_filho(self.match('Palavra Reservada'))  # 'take'
+                    no_escopo.adicionar_filho(self.match('Num Inteiro'))  # '0'
+                    self.match('Caractere Especial')  # ';'
+                else:
+                    raise SyntaxError(f"Token inesperado {token} na linha {self.tokens[self.indice_token_atual]['linha']}")
 
-        no_pai.adicionar_filho(no_escopo)
+            no_pai.adicionar_filho(no_escopo)
+
+        except SyntaxError as e:
+            print(e)
 
     def analisar_ordo(self):
         no_ordo = No('Ordo')
@@ -149,10 +262,24 @@ class AnalisadorSintatico:
 
         no_pai.adicionar_filho(no_desvio2)
 
-def imprimir_arvore_sintatica(no, nivel=0):
-    print("  " * nivel + f"{no.tipo}: {no.lexema}")
-    for filho in no.filhos:
-        imprimir_arvore_sintatica(filho, nivel + 1)
+def imprimir_arvore_sintatica(no, nivel=0, lado=None):
+    if no.lexema is not None:
+        if nivel > 0:
+            prefixo = "|   " * (nivel - 1) + "+--"
+        else:
+            prefixo = ""
+        print(f"{prefixo} {no.tipo}: {no.lexema} ({lado})")
+    else:
+        print(" " * nivel + f"{no.tipo}")
+
+    for i, filho in enumerate(no.filhos):
+        novo_lado = None
+        if i == len(no.filhos) - 1:
+            novo_lado = "D"
+        elif i == 0:
+            novo_lado = "E"
+
+        imprimir_arvore_sintatica(filho, nivel + 1, lado=novo_lado)
 
 def main():
     path = "controlefluxo.txt"
@@ -162,8 +289,6 @@ def main():
     # Chamada para imprimir a árvore sintática
     print("\nÁrvore Sintática:")
     imprimir_arvore_sintatica(analisador_sintatico.raiz)
-
-    
 
 if __name__ == "__main__":
     main()
---
+++ python/sintatico3.py
@@ -0,0 +1,363 @@
+#### GRAMÁTICA ###
+
+# Início
+# main  ListaDeDeclaração  < escopo >
+
+# Lista De Declaração
+# ListaDeDeclaração | declaração | vacuum
+
+# Declaração
+# tipoVar variavel ;
+
+# TipoVar
+# text | numero 
+
+# Número
+# num_int | num_flu
+
+# Variável 
+# ID
+
+# Escopo 
+# escopo | comando | ε
+
+# Comando 
+# entrada | saida | desvio | atribuicao | laco
+
+# Entrada
+# textin [ variavel ]
+
+# Saída
+# textout [ texto | expAritmetica ]
+
+# Exp Aritmética 
+# exp | termo
+
+# Desvio 
+# case( exp ) < escopo > desvio2
+
+# Atribuição
+# variavel conteudo ;
+
+# Conteúdo
+# texto | exp
+
+# Laço
+# to( atribuicao conteudo simbolo_relacional conteudo ; variavel << expAritmetica ) < escopo > | when( exp ) < escopo > | take;
+
+# Exp
+# logico | ok | notok
+
+# Lógico
+# expressãoLogica termo logico
+
+# Termo
+# fator termo2
+
+# Termo2
+# *fator termo2 | / fator termo2 | // fator termo2 | ε
+
+# Fator
+# ( expAritmetica ) | variavel | numero | função
+
+# Função
+# ID ( argumento )
+
+# Argumento
+# expAritmetica argumento2 | ε
+
+# Argumento2
+# expAritmetica argumento2 | ε
+
+# Texto
+# text
+
+# Termo lógico
+# || expressaoLogica termoLogico | ε
+
+# Expressão Lógica
+# expressaoLogica3 expressaoLogica2
+
+# Expressão Lógica2
+# && expressaoLogica3 expressaoLogica2 | ε
+
+# Expressão Lógica3
+# ! relacional | relacional
+
+# Simbolo_relacional
+# << | >> | <<< | >>> | == | != | ok | notok
+
+# Desvio2
+# caseNot < escopo > | caseNot desvio3
+
+# Desvio3
+# case ( exp ) < escopo > desvio2 | ε
+
+# Relacional
+# logico | termoRelacional
+
+# TermoRelacional
+# conteudo termoRelacional2
+
+# TermoRelacional2
+# simbolo_relacional conteudo | ε
+
+from lexico2 import AnalisadorLexico
+
+class Node:
+    def __init__(self, label, children=None):
+        self.label = label
+        self.children = children or []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+
+class AnalisadorSintatico:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.posicao = 0
+
+    def parse(self):
+        return self.main()
+
+    def match(self, tipo_esperado):
+        if self.posicao < len(self.tokens) and self.tokens[self.posicao]['tipo'] == tipo_esperado:
+            self.posicao += 1
+            return True
+        return False
+
+    def main(self):
+        node = Node('main')
+        if (
+            self.match('Palavra Reservada')
+            and self.match('main')
+            and self.match('Palavra Reservada')
+            and self.match('vacuum')
+            and self.match('<')
+            and self.lista_declaracao()
+            and self.escopo()
+            and self.match('>')
+        ):
+            return node
+        return None
+
+    def lista_declaracao(self):
+        node = Node('ListaDeDeclaracao')
+        child = self.declaracao()
+        while self.match('|'):
+            child = self.declaracao()
+            if not child:
+                return None
+            node.add_child(child)
+        return node
+
+    def declaracao(self):
+        tipo_var_node = self.tipo_var()
+        variavel_node = self.variavel()
+
+        if tipo_var_node and variavel_node and self.match(';'):
+            node = Node('Declaracao')
+            node.add_child(tipo_var_node)
+            node.add_child(variavel_node)
+            return node
+
+        return None
+
+    def tipo_var(self):
+        return self.match('Palavra Reservada')
+
+    def variavel(self):
+        return self.match('Identificador')
+
+    def escopo(self):
+        node = Node('escopo')
+        if self.match('Palavra Reservada'):
+            child = self.lista_declaracao()
+            if child:
+                node.add_child(child)
+                child = self.escopo()
+                if child:
+                    node.add_child(child)
+                    return node
+        elif self.lista_declaracao():
+            return Node('ListaDeDeclaracao')
+        return None
+    
+    def comando(self):
+        if self.entrada() or self.saida() or self.desvio() or self.atribuicao() or self.laco():
+            return True
+        return False
+
+    def entrada(self):
+        if self.match('textin') and self.match('[') and self.variavel() and self.match(']'):
+            return True
+        return False
+
+    def saida(self):
+        if self.match('textout') and self.match('[') and (self.texto() or self.exp_aritmetica()) and self.match(']'):
+            return True
+        return False
+
+    def exp_aritmetica(self):
+        return self.exp() or self.termo()
+
+    def desvio(self):
+        if self.match('case') and self.match('(') and self.exp() and self.match(')') and self.match('<') and self.escopo() and self.match('>') and self.desvio2():
+            return True
+        return False
+
+    def atribuicao(self):
+        if self.variavel() and self.conteudo() and self.match(';'):
+            return True
+        return False
+
+    def conteudo(self):
+        return self.texto() or self.exp()
+
+    def laco(self):
+        if self.match('to') and self.match('(') and self.atribuicao() and self.conteudo() and self.simbolo_relacional() and self.conteudo() and self.match(';') and self.variavel() and self.match('<<') and self.exp_aritmetica() and self.match(')') and self.match('<') and self.escopo() and self.match('>'):
+            return True
+        elif self.match('when') and self.match('(') and self.exp() and self.match(')') and self.match('<') and self.escopo() and self.match('>'):
+            return True
+        elif self.match('take'):
+            return True
+        return False
+
+    def exp(self):
+        return self.logico() or self.match('ok') or self.match('notok')
+
+    def logico(self):
+        return self.expressao_logica() and self.termo_logico()
+
+    def termo(self):
+        return self.fator() and self.termo2()
+
+    def termo2(self):
+        if self.match('*') and self.fator() and self.termo2():
+            return True
+        elif self.match('/') and self.fator() and self.termo2():
+            return True
+        elif self.match('//') and self.fator() and self.termo2():
+            return True
+        return False
+
+    def fator(self):
+        if self.match('(') and self.exp_aritmetica() and self.match(')'):
+            return True
+        elif self.variavel() or self.numero() or self.funcao():
+            return True
+        return False
+
+    def funcao(self):
+        if self.match('Identificador') and self.match('(') and self.argumento() and self.match(')'):
+            return True
+        return False
+
+    def argumento(self):
+        return self.exp_aritmetica() and self.argumento2()
+
+    def argumento2(self):
+        if self.exp_aritmetica() and self.argumento2():
+            return True
+        return False
+
+    def texto(self):
+        return self.match('String')
+
+    def termo_logico(self):
+        if self.match('||') and self.expressao_logica() and self.termo_logico():
+            return True
+        return False
+
+    def expressao_logica(self):
+        return self.expressao_logica3() and self.expressao_logica2()
+
+    def expressao_logica2(self):
+        if self.match('&&') and self.expressao_logica3() and self.expressao_logica2():
+            return True
+        return False
+
+    def expressao_logica3(self):
+        if self.match('!') and self.relacional():
+            return True
+        elif self.relacional():
+            return True
+        return False
+
+    def simbolo_relacional(self):
+        return self.match('<<') or self.match('>>') or self.match('<<<') or self.match('>>>') or self.match('==') or self.match('!=') or self.match('ok') or self.match('notok')
+
+    def desvio2(self):
+        if self.case_not() and self.match('<') and self.escopo() and self.match('>'):
+            return True
+        elif self.case_not() and self.desvio3():
+            return True
+        return False
+
+    def desvio3(self):
+        if self.match('case') and self.match('(') and self.exp() and self.match(')') and self.match('<') and self.escopo() and self.match('>') and self.desvio2():
+            return True
+        return True
+
+    def relacional(self):
+        return self.logico() or self.termo_relacional()
+
+    def termo_relacional(self):
+        return self.conteudo() and self.termo_relacional2()
+
+    def termo_relacional2(self):
+        if self.simbolo_relacional() and self.conteudo():
+            return True
+        return False
+
+    def case_not(self):
+        return self.match('caseNot')
+    
+    def imprimir_arvore(self, node, nivel=0):
+        if node:
+            print('  ' * nivel + f'{node.label}')
+            for child in node.children:
+                self.imprimir_arvore(child, nivel + 1)
+
+tokens_exemplo = [
+    {'tipo': 'Palavra Reservada', 'lexema': 'main'},
+    {'tipo': 'Palavra Reservada', 'lexema': 'num_int'},
+    {'tipo': 'Identificador', 'lexema': 'x'},
+    {'tipo': ';', 'lexema': ';'},
+    {'tipo': 'vacuum', 'lexema': 'vacuum'},
+    {'tipo': '<', 'lexema': '<'},
+    {'tipo': 'Identificador', 'lexema': 'x'},
+    {'tipo': '>', 'lexema': '>'},
+]
+
+analisador_sintatico = AnalisadorSintatico(tokens_exemplo)
+arvore_sintatica = analisador_sintatico.parse()
+
+if arvore_sintatica:
+    print("Análise sintática bem-sucedida! Árvore sintática gerada:")
+    analisador_sintatico.imprimir_arvore(arvore_sintatica)
+else:
+    print("Erro na análise sintática.")
+
+if __name__ == "__main__":
+    analisador_lexico = AnalisadorLexico("fibonacci.txt")
+    tokens_controle_fluxo = analisador_lexico.get_tabela_simbolos()
+
+    if tokens_controle_fluxo:
+        print("Análise léxica bem-sucedida! Tokens gerados:")
+        analisador_lexico.imprimir_tokens()
+    else:
+        print("Erro na análise léxica.")
+
+    # Agora, vamos usar os tokens gerados na análise sintática
+    analisador_sintatico = AnalisadorSintatico(tokens_controle_fluxo)
+    arvore_sintatica_controle_fluxo = analisador_sintatico.parse()
+
+    if arvore_sintatica_controle_fluxo:
+        print("\nAnálise sintática bem-sucedida! Árvore sintática gerada:")
+        analisador_sintatico.imprimir_arvore(arvore_sintatica_controle_fluxo)
+    else:
+        print("\nErro na análise sintática.")
+
+
